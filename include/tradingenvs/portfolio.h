e45c55698156d83681183c60d3befb299ab2298c
quaere-verum <quaereverum2538@gmail.com>
2024-05-03 22:47:49 +0200

    Starting adding env logic

--- include/tradingenvs/portfolio.h
+++ include/tradingenvs/portfolio.h
@@ -2,36 +2,43 @@
 #include <vector>
 
 struct Position {
-	Position(const int& position_, const float& entryPrice_, const float& amount_, const float& takeProfit_, const float& stopLoss_, const int& holdTime_) :
-	position(position_), entryPrice(entryPrice_), amount(amount_), takeProfit(takeProfit_), stopLoss(stopLoss_), holdTime(holdTime_){}
+	Position(const int& position_, const float& entryPrice_, const float& amount_, 
+		const float& takeProfit_, const float& stopLoss_, const int& holdTime_, const float& leverageMult_) :
+	position(position_), entryPrice(entryPrice_), amount(amount_), takeProfit(takeProfit_), stopLoss(stopLoss_), holdTime(holdTime_), leverageMult(leverageMult_){}
 	int position;
 	float entryPrice;
 	float amount;
 	float takeProfit;
 	float stopLoss;
 	int holdTime;
+	float leverageMult;
 	int timeOpen = 0;
 	
 
 };
 
-class Portfolio {
+class MarginPortfolio {
 public:
-	Portfolio(float initialCapital, float tradingFees_) : capital(initialCapital), tradingFees(tradingFees_){};
+	MarginPortfolio(float initialCapital_, float tradingFees_): 
+		capital(initialCapital_),
+		initialCapital(initialCapital_),
+		tradingFees(tradingFees_)
+	{};
 
 	std::vector<Position> positions;
 	float capital;
+	const float initialCapital;
 	const float tradingFees;
 
-	void openPosition(int position, float price, float amount, float takeProfit, float stopLoss) {
-		capital -= amount * price*(1+tradingFees)*position;
-		positions.push_back(Position(position, price, amount, takeProfit, stopLoss))
+	void openPosition(int position, float price, float amount, float takeProfit, float stopLoss, int holdTime, float leverageMult) {
+		capital -= amount * price*(1+position*tradingFees)*position;
+		positions.push_back(Position(position, price, amount, takeProfit, stopLoss, holdTime, leverageMult));
 	}
 
 	float valuate(float price) {
 		float valuation = capital;
 		for (int i = 0; i < positions.size(); i++) {
-			valuation += positions[i].position*(price*positions[i].amount)
+			valuation += positions[i].leverageMult * positions[i].position * (price * positions[i].amount);
 		}
 		return valuation;
 	}
@@ -40,11 +47,15 @@ public:
 		float positionReturn;
 		for (int i = 0; i < positions.size(); i++) {
 			positions[i].timeOpen += 1;
-			positionReturn = (positions[i].entryPrice - price) / price - 1;
+			positionReturn = ((positions[i].entryPrice - price) / price - 1)*positions[i].leverageMult;
 			if (positionReturn < positions[i].stopLoss || positionReturn > positions[i].takeProfit || positions[i].timeOpen >= positions[i].holdTime)
-				capital += positions[i].amount * price * positions[i].position * (1 - tradingFees);
+				capital += positions[i].leverageMult*positions[i].amount * price * positions[i].position * (1 - positions[i].position*tradingFees);
 				positions.erase(positions.begin() + i);
-
 		}
+	}
+
+	void reset() {
+		capital = initialCapital;
+		positions.clear();
 	}
 };
--- include/tradingenvs/tradingenv.h
+++ include/tradingenvs/tradingenv.h
@@ -7,8 +7,47 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <cmath>
+#include <tradingenvs/portfolio.h>
+#include <vector>
 
-class TradingEnv {
-	public:
-		TradingEnv()
+namespace py = pybind11;
+
+class MarginTradingEnv {
+public:
+	MarginTradingEnv(float initialCapital_, float tradingFees_) : 
+		initialCapital(initialCapital_),
+		tradingFees(tradingFees_),
+		portfolio(MarginPortfolio(initialCapital_, tradingFees_))
+	{};
+
+	MarginPortfolio portfolio;
+	float initialCapital;
+	float tradingFees;
+	int step;
+	int t;
+	std::vector<float> priceSeries;
+	std::vector<int> takeAction;
+
+	int reset(std::vector<float> priceSeries_, std::vector<int> takeAction_) {
+		priceSeries = priceSeries_;
+		takeAction = takeAction_;
+		portfolio.reset();
+		t, step = 0;
+		int act;
+		while (true) {
+			if (t >= takeAction.size()) {
+				return -1;
+			}
+			act = takeAction[t];
+			if (act == 1) {
+				return t;
+			}
+			t++;
+		}
+	}
+
+	std::tuple<int, float> step(py::array_t<float> action) {
+		return;
+	}
+
 };
